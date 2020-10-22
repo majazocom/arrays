fbfaafd415828bafe79867ea0e94e3f3037a4c82
Maja Gedda <maja.gedda@zocom.se>
2020-10-22 12:29:59 +0200

    solution

--- script.js
+++ script.js
@@ -44,26 +44,40 @@ let pokemons = [
 
 //Del 1:
 //Ändra namn på det första objektet
-
+console.log(pokemons[0].name);
+pokemons[0].name = "Maja";
+console.log(pokemons[0].name);
 
 //Del 2:
 //Spara det andra objektet i en variabel
-
+let variable = pokemons[1];
+console.log(variable);
 
 //Del 3:
 //Skriv ut listans längd
-
+console.log(pokemons.length);
 
 //Del 4:
 //Radera sista objektet i listan
-
+pokemons.pop();
+console.log(pokemons.length);
 
 //Del 5:
 //Lägg in ett till objekt i listan
+let newObject = {
+    name: "Hej",
+    id: 100
+}
 
+pokemons.push(newObject);
+console.log(pokemons.length);
 
 //Del 6:
 //Ta bort det första objektet i listan
+pokemons.shift();
+console.log(pokemons.length);
+
 
 //Del 7:
 //Kom åt det sista objektet genom listmetoden '.length'
+console.log(pokemons[pokemons.length -1]);
