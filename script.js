5ab6d04d44ea8586d1b1e7fa7ff01965a27c4531
Maja Gedda <maja.gedda@zocom.se>
2020-10-22 10:59:53 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,66 @@
+//Simple array
+let pokemons = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise"];
+
+
+//Objects in an array
+let pokemons = [
+    {
+        name: 'Bulbasaur',
+        id: 1
+    },
+    {
+        name: 'Ivysaur',
+        id: 2
+    },
+    {
+        name: 'Venusaur',
+        id: 3
+    },
+    {
+        name: 'Charmander',
+        id: 4
+    },
+    {
+        name: 'Charmeleon',
+        id: 5
+    },
+    {
+        name: 'Charizard',
+        id: 6
+    },
+    {
+        name: 'Squirtle',
+        id: 7
+    },
+    {
+        name: 'Wartortle',
+        id: 8
+    },
+    {
+        name: 'Blastoise',
+        id: 9
+    },
+]
+
+//Del 1:
+//Ändra namn på det första objektet
+
+
+//Del 2:
+//Spara det andra objektet i en variabel
+
+
+//Del 3:
+//Skriv ut listans längd
+
+
+//Del 4:
+//Radera sista objektet i listan
+
+
+//Del 5:
+//Lägg in ett till objekt i listan
+
+
+//Del 6:
+//Ta bort det första objektet i listan
